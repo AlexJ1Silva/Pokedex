42db8f570cb3dc669911c3a68b0bc4f105e8b1dd
Alex J. Silva <111905692+AlexJ1Silva@users.noreply.github.com>
2023-04-01 21:00:23 -0300

    v2 adicionado filtro de pesquisa

--- pokedex.js
+++ pokedex.js
@@ -1,69 +1,74 @@
 const pokeContainer = document.querySelector("#pokeContainer");
-
-
-const pokemonCount = 2000;
-
+const select = document.getElementById("pesquisar");
+const numeroPokemon = document.getElementById("range");
+const pokemonCount = 1120;
+let allData = [];
 const tipoCor = {
-    fire: "#FF6C53",
-    grass: "#AEFFED",
-    electric: "#F2FF80",
-    water: "#53D9FF",
-    ground: "#DD6F00",
-    rock: "#BBCAB7",
-    fairy: "#FFAEFD",
-    poison: "#C671AA",
-    bug: "#7DFF94",
-    dragon: "#97b3e6",
-    psychic: "#5AEFBF",
-    flying: "#CAE2FF",
-    fighting: "#EEC0B0",
-    normal: "#F5F5F5",
+  fire: "#FF6C53",
+  grass: "#AEFFED",
+  electric: "#F2FF80",
+  water: "#53D9FF",
+  ground: "#DD6F00",
+  rock: "#BBCAB7",
+  fairy: "#FFAEFD",
+  poison: "#C671AA",
+  bug: "#7DFF94",
+  dragon: "#97b3e6",
+  psychic: "#5AEFBF",
+  flying: "#CAE2FF",
+  fighting: "#EEC0B0",
+  normal: "#F5F5F5",
 };
 
 const baseTipos = Object.keys(tipoCor);
 
 const fetchPokemon = async () => {
-    for (let i = 1; i <= pokemonCount; i++) {
-        await getPokemons(i);
-    }
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getPokemons(i);
+
+  }
 };
 
 const getPokemons = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const resp = await fetch(url);
-    const data = await resp.json();
-    criaPokemonCartas(data);
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  allData.push(data);
+  criaPokemonCartas(data);
 };
 
 const criaPokemonCartas = (poke) => {
-  
-   
-    
+  const name = poke.name[0].toUpperCase() + poke.name.slice(1).split("-", 1);
 
-    const name = poke.name[0].toUpperCase() + poke.name.slice(1).split("-",1);
+  const id = poke.id.toString().padStart(3, "0");
 
-    const id = poke.id.toString().padStart(3, "0");
+  const pokeTipos = poke.types.map((type) => type.type.name);
+  console.log(pokeTipos);
 
-    const pokeTipos = poke.types.map(type => type.type.name);
+  //console.log(value)
+  const type = baseTipos.find((type) => pokeTipos.indexOf(type) > -1);
 
-    const type = baseTipos.find(type => pokeTipos.indexOf(type) > -1);
+  let tipos = "";
 
-    const cor = tipoCor[type];
+  poke.types.forEach((e) => {
+    tipos = !tipos ? e.type.name : tipos + " / " + e.type.name;
+  });
 
-    const cartas = document.createElement('div');
-    cartas.classList.add("borda", type);
-    
-    
+  const cor = tipoCor[type];
 
-    const borda = document.createElement('div');
-    borda.classList.add("pokemon")
+  //console.log(cor)
 
-    cartas.style.backgroundColor = cor;
+  //console.log(select)
 
+  const cartas = document.createElement("div");
+  cartas.classList.add("borda", type);
 
+  const borda = document.createElement("div");
+  borda.classList.add("pokemon");
 
-    const pokemonInnerHTML = 
-    `
+  cartas.style.backgroundColor = cor;
+
+  const pokemonInnerHTML = `
     <div class="pokemon">    
     <div class="imgContainer">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png " alt="${name}">
@@ -71,15 +76,44 @@ const criaPokemonCartas = (poke) => {
     <div class="info">
         <span class="number">#${id}</span>
         <h3 class="name" >${name}</h3>
-    <div class="small" id="type">Type: <span><b>${type}</b></span>
+    <div class="small" id="type">Type: <span><b>${tipos}</b></span>
     </div>
     </div>
     </div> `;
 
-    cartas.innerHTML = pokemonInnerHTML
+  cartas.innerHTML = pokemonInnerHTML;
 
-    pokeContainer.appendChild(cartas)
-     
+  pokeContainer.appendChild(cartas);
 };
 
-fetchPokemon()
+function pesquisar() {
+  pokeContainer.innerHTML = "";
+
+  const value = select.options[select.selectedIndex].value;
+
+  if (value == "todos") {
+    allData.forEach((e) => {
+      criaPokemonCartas(e);
+    });
+  } else {
+    let filtro = [];
+
+    allData.forEach((e) => {
+      e.types.forEach((t) => {
+        if (t.type.name == value) {
+          filtro.push(e);
+        }
+      });
+    });
+
+    //console.log(filtro)
+    //criaPokemonCartas(filtro);
+
+    filtro.forEach((element) => {
+      //console.log(element)
+      criaPokemonCartas(element);
+    });
+  }
+}
+
+fetchPokemon();
