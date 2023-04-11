1bc0cf2adf8484f2df0be3eb3e276e9f33c55867
Alex J. Silva <111905692+AlexJ1Silva@users.noreply.github.com>
2023-04-11 11:27:59 -0300

    Update pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -139,20 +139,4 @@ function pesquisar() {
   
 }
 
-/* function update() {
-
-    var element = document.getElementById("myprogressBar");   
-    var width = 1;
-    var identity = setInterval(scene, 9);
-    function scene() {
-      if (width >= 100) {
-        clearInterval(identity);
-      } else {
-        width++; 
-        element.style.width = width + '%'; 
-        element.innerHTML = width * 1   + '%';
-      }
-    }
-  } */
-
 fetchPokemon();
